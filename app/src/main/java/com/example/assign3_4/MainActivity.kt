de70eb9de7c58e150db6a03078d1a4a0bf2744ac
alicehan8 <alicehan@bu.edu>
2025-09-30 13:18:50 -0400

    completed

--- app/src/main/java/com/example/assign3_4/MainActivity.kt
+++ app/src/main/java/com/example/assign3_4/MainActivity.kt
@@ -5,13 +5,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.assign3_4.ui.theme.Assign3_4Theme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,15 +33,59 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Assign3_4Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ScaffoldExample()
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScaffoldExample(){
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Scaffold Example", style = MaterialTheme.typography.titleLarge) }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.fillMaxWidth()
+            ){
+                Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f).padding(10.dp)){
+                    Text("Home")
+                }
+                Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f).padding(10.dp)){
+                    Text("Settings")
+                }
+                Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f).padding(10.dp)){
+                    Text("Profile")
+                }
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar("Button Clicked!", actionLabel = "Dismiss")
+                    }
+                }){
+                Text("Click me!")
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center,
+        snackbarHost = {
+            SnackbarHost(snackbarHostState)
+        },
+        content = { innerPadding ->
+            Text("Hello Scaffold!", modifier = Modifier.padding(innerPadding))
+        }
+    )
 }
 
 @Composable
@@ -42,6 +100,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Assign3_4Theme {
-        Greeting("Android")
+        ScaffoldExample()
     }
 }
